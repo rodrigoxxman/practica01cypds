25d3fd2e6df6ef84a8c421cb9b170306a9e01bf1
Alumno <Alumno@LAB701-10>
2018-03-05 10:36:18 -0500

    esqueleto del proyecto

---
+++ app/src/main/java/prueba1/app/App.java
@@ -0,0 +1,13 @@
+package prueba1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
