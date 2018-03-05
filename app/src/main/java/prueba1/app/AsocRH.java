ab618615062e3c77d8bf3741f8e8012332d25f0e
Alumno <Alumno@LAB701-09>
2018-03-05 11:30:53 -0500

    Cambio Alexis

---
+++ app/src/main/java/prueba1/app/AsocRH.java
@@ -0,0 +1,8 @@
+package prueba1.app;
+
+public class AsocRH {
+	String [] nom_pren = { "Polo", "Casaca","Pantalón","Zapatillas"};
+	String [] descrip = { "Manga corta", "Delgada o gruesa", "Vestir", "Deporte o todo terreno"};
+	String [] talla = { "S", "M", "L", "XL"};
+	double [] precio = { 20, 32, 27, 35};
+}
---
+++ app/src/main/java/prueba1/app/AsocRN.java
@@ -0,0 +1,8 @@
+package prueba1.app;
+
+public class AsocRN {
+	String [] nom_pren = { "Polo", "Casaca", "Pantalón", "Zapatillas"};
+	double [] precio = { 20, 32, 27, 35};
+	String [] fotos = {"Cargando", "Cargando", "Cargando", "Cargando"};
+	
+}
