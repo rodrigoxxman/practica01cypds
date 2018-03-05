fc7be18f8a1e0b7b54ce7b68a2957866c91df263
Alumno <Alumno@LAB701-10>
2018-03-05 11:22:33 -0500

    Metodos Salas

---
+++ app/src/main/java/prueba1/app/AsocRB.java
@@ -0,0 +1,7 @@
+package prueba1.app;
+
+public class AsocRB {
+String [] nombreprenda = {"test1","test2","test3","test4","test5","test6"};
+String [] descripcion = {"test1","test2","test3","test4","test5","test6"};
+int [] talla = {2,3,5,6,7};
+}
---
+++ app/src/main/java/prueba1/app/AsocRD.java
@@ -0,0 +1,9 @@
+package prueba1.app;
+public class AsocRD {
+	String [] nompren = {"test1","test2","test3","test4","test5","test6"};
+	String [] descrip = {"testdescrip1","testdescrip2","testdescrip3","testdescrip4",
+			"testdescrip5","testdescrip6"};
+	char [] talla = {2,3,5,6,7};
+	Double [] precio = {20.5, 35.5 ,15.8, 60.7};
+	
+}
